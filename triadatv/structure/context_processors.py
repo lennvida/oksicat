fc6c1f7b9725202ba104e6c427362dca6370fde5
Oxymoron42 <dimka.n42@gmail.com>
2013-04-13 03:02:04 -0700

    подстройка под специфику koding.com

--- triadatv/structure/context_processors.py
+++ triadatv/structure/context_processors.py
@@ -12,8 +12,8 @@ def site_name(request):
 
 def current_node(request):
     current_root_node = current_top_node = current_node = StructureNode.objects.get_by_path(request.path)
-    if current_node and current_node.get_ancestors().filter(level=1).count() > 0:
-        current_root_node = current_node.get_ancestors().filter(level=1)[0]
+    if current_node and current_node.get_ancestors().filter(level=2).count() > 0:
+        current_root_node = current_node.get_ancestors().filter(level=2)[0]
     if current_node and current_node.get_ancestors().filter(level=3).count() > 0:
         current_top_node = current_node.get_ancestors().filter(level=3)[0]
     return {
